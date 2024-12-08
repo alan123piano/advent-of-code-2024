396e1888bbdfa6594f4c74141153c4c8273d73a0
Alan Yang <alan123piano@gmail.com>
2024-12-08 02:31:01 -0500

    day 4, part 2

--- app/Day4.hs
+++ app/Day4.hs
@@ -32,30 +32,62 @@ parseWordSearch s =
         (Map.empty :: Map (Int, Int) Char)
         (zip [1 ..] ls)
 
--- returns whether `word` is in the word search starting from (r, c) and going in direction (dr, dc)
-findWord :: WordSearch -> String -> (Int, Int) -> (Int, Int) -> Bool
-findWord ws word (dr, dc) =
-  reducer word
+-- returns whether the provided "form" is in the word search at (r, c)
+findForm :: WordSearch -> [(Char, (Int, Int))] -> (Int, Int) -> Bool
+findForm ws forms (r, c) =
+  all findChar forms
   where
-    reducer (chr : word') (r, c) = case Map.lookup (r, c) (wsGrid ws) of
-      Just chr' | chr == chr' -> reducer word' (r + dr, c + dc)
-      _ -> False
-    reducer [] _ = True
+    findChar (chr, (dr, dc)) = Just chr == Map.lookup (r + dr, c + dc) (wsGrid ws)
 
--- all eight directions of movement on a grid
-allDirections :: [(Int, Int)]
-allDirections = filter (\(dr, dc) -> dr /= 0 || dc /= 0) [(dr, dc) | dr <- [-1 .. 1], dc <- [-1 .. 1]]
+xmasForms :: [[(Char, (Int, Int))]]
+xmasForms =
+  [[(chr, (dr * i, dc * i)) | (i, chr) <- zip [0 ..] "XMAS"] | (dr, dc) <- allDirections]
+  where
+    -- all eight directions of movement on a grid
+    allDirections :: [(Int, Int)]
+    allDirections =
+      filter
+        (\(dr, dc) -> dr /= 0 || dc /= 0)
+        [(dr, dc) | dr <- [-1 .. 1], dc <- [-1 .. 1]]
+
+addVec2 :: (Int, Int) -> (Int, Int) -> (Int, Int)
+addVec2 (r1, c1) (r2, c2) = (r1 + r2, c1 + c2)
+
+scaleVec2 :: Int -> (Int, Int) -> (Int, Int)
+scaleVec2 mul (r, c) = (r * mul, c * mul)
+
+negVec2 :: (Int, Int) -> (Int, Int)
+negVec2 = scaleVec2 (-1)
+
+crossMasForms :: [[(Char, (Int, Int))]]
+crossMasForms =
+  [ [ ('A', (0, 0)),
+      ('M', addVec2 (negVec2 right) (negVec2 down)),
+      ('M', addVec2 (negVec2 right) down),
+      ('S', addVec2 right (negVec2 down)),
+      ('S', addVec2 right down)
+    ]
+    | (right, down) <-
+        [ ((0, 1), (1, 0)),
+          ((1, 0), (0, -1)),
+          ((0, -1), (-1, 0)),
+          ((-1, 0), (0, 1))
+        ]
+  ]
 
 allPoints :: WordSearch -> [(Int, Int)]
 allPoints ws = [(r, c) | r <- [1 .. wsNRows ws], c <- [1 .. wsNCols ws]]
 
 part1 :: String -> Int
 part1 contents =
-  length $ filter (uncurry (findWord ws "XMAS")) allSearches
+  length $ filter (uncurry (findForm ws)) allSearches
   where
     ws = parseWordSearch contents
-    allSearches = [(dpos, pos) | pos <- allPoints ws, dpos <- allDirections]
+    allSearches = [(form, pos) | form <- xmasForms, pos <- allPoints ws]
 
 part2 :: String -> Int
 part2 contents =
-  undefined
+  length $ filter (uncurry (findForm ws)) allSearches
+  where
+    ws = parseWordSearch contents
+    allSearches = [(form, pos) | form <- crossMasForms, pos <- allPoints ws]
